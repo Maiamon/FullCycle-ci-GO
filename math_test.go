1269bfd2bc172302eae976872370bbeba03e1d62
Maiamon <gsmaia23@gmail.com>
2025-02-06 17:10:29 -0400

    [ci] Add github actions for every push
    [Fix] ci
    
    [fix] change name of directory
    
    [fix] ci
    
    [fix] adding go mod and fixing math_test
    
    [Fix] go version on ci

--- math_test.go
+++ math_test.go
@@ -1,7 +1,9 @@
 package main
 
+import "testing"
+
 func TestSoma(t *testing.T) {
-	resultado := Soma(10, 5)
+	resultado := Somar(10, 5)
 	if resultado != 15 {
 		t.Errorf("Soma(10, 5) = %d; want 15", resultado)
 	}
